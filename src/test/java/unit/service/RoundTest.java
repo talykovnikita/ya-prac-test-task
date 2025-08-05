050df73cc0cd232b0d0a91eec5c932cc05d673a0
Talykov Nikita <talykovnikita@gmail.com>
2025-08-06 01:46:18 +0300

    small refactor

--- src/test/java/unit/service/RoundTest.java
+++ src/test/java/unit/service/RoundTest.java
@@ -3,6 +3,7 @@ package unit.service;
 
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.talykov.exception.RoundException;
 import ru.talykov.service.RoundService;
@@ -17,6 +18,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 public class RoundTest {
 
     @Test
+    @DisplayName("Проверка округления в большую сторону")
     void roundUpTest() {
         assertThat(RoundService.round(BigDecimal.valueOf(3.14)))
                 .as("Неверное округление")
@@ -24,6 +26,7 @@ public class RoundTest {
     }
 
     @Test
+    @DisplayName("Проверка выброса исключения при попытке округлить `null`")
     void roundNullValueTest() {
         assertThatThrownBy(() -> RoundService.round(null))
                 .isInstanceOf(RoundException.class)
